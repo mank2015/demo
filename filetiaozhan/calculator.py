fc5a7fab573667b14d3a7332153bf3bf66de763d
847749636 <847749636@qq.com>
2017-10-23 08:15:54 +0800

    update dict file

--- filetiaozhan/calculator.py
+++ filetiaozhan/calculator.py
@@ -29,13 +29,14 @@ class Config(object):
         self._configfile = configfile
     def filepwd(self):
         return self._configfile
-    def fileread(self):
+    def get_config(self):
         with open(self.filepwd()) as file:
-            print(file.readline())
+            self._config = dict(line.strip().split('=') for line in file if line)
+            print(self._config)
 
 if __name__ == '__main__':
     pwdfile = findpwd(sys.argv[1:])
     m_pwd = os.path.join('/home/shiyanlou/demo/filetiaozhan',pwdfile.c_pwd())
     config = Config(m_pwd)
-    print(config.fileread())
+    print(config.get_config())
     print(m_pwd)
