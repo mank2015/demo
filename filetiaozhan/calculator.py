10568396489cd29f4ad0cf43b549fed379cc2fba
847749636 <847749636@qq.com>
2017-10-23 23:40:05 +0800

    201710232337

--- filetiaozhan/calculator.py
+++ filetiaozhan/calculator.py
@@ -50,22 +50,60 @@ class UserData(object):
         with open(self._userdatafile) as file:
             self.userdata = dict(line.strip().split(',') for line in file if line)
         return self.userdata
+    def baoxian(self, gongzi):
+        baoxianfei =  gongzi * (0.08+0.02+0.005+0.06)
+        return baoxianfei
     def calculator(self):
+        sala= []
         for k,value in self.salary().items():
-            try:
-            jobnu,salary = item.split(':')
-            salary = int(salary)
-        except ValueError:
-            print("Parameter Error")
-        else:
+            salary = int(value)
+        
             if salary > 3500:
-                inc = salary - baoxian(salary) - 3500
-                yinnashui(inc)
-                print(jobnu +':'+ '{:.2f}'.format(salary-yinnashui(inc)-baoxian(salary)))
+                inc = salary - self.baoxian(salary) - 3500
+                calcul = salary-self.yinnashui(inc)-self.baoxian(salary)
+                sala.append((k, calcul))   
+                            
+#innashui(inc)
+               # print(k +':'+ '{:.2f}'.format(salary-self.yinnashui(inc)-self.baoxian(salary)))
             else:
-                inc = salary - baoxian(salary)
-                print(jobnu +':'+ '{:.2f}'.format(inc)) 
-                
+                inc = salary - self.baoxian(salary)
+                sala.append((k, inc))
+                #print(k +':'+ '{:.2f}'.format(inc)) 
+            print(sala)
+            return sala
+    def yinnashui(self, income):
+        level = 0.03
+        onelevel = 0.1
+        twolevel = 0.2
+        threelevel = 0.25
+        fourlevel = 0.3
+        fivelevel = 0.35
+        sixlevel = 0.45
+
+        deduct = 0
+        onededuct = 105
+        twodeduct = 555
+        threededuct = 1055
+        fourdeduct = 2755
+        fivededuct = 5505
+        sixdeduct = 13505
+        if 0 < income <= 1500:
+            taxable = (income * level) - deduct
+        elif income <= 0:
+            taxable = 0
+        elif 1500 < income <= 4500:
+            taxable = (income * onelevel) - onededuct
+        elif 4500 < income <= 9000:
+            taxable = (income * twolevel) - twodeduct
+        elif 9000 < income <= 35000:
+            taxable = (income * threelevel) - threededuct
+        elif 35000 < income <= 55000:
+            taxable = (income * fourlevel) - fourdeduct
+        elif 55000 < income <= 80000:
+            taxable = (income * fivelevel) - fivededuct
+        elif 80000 < income:
+            taxable = (income * sixlevel) - sixdeduct
+        return taxable
 
 if __name__ == '__main__':
     pwdfile = findpwd(sys.argv[1:])
